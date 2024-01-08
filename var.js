f4231c28522e7e6c5502aba13fdb20636eedfa50
claudioalejandroramirez <claudioramirez.ti@gmail.com>
2024-01-08 09:21:14 -0300

    var1

---
+++ var.js
@@ -0,0 +1,16 @@
+//estudo java script
+//string
+var texto = 'Curso de JavasCript'
+
+//number (inteiro(int) ou fracionado(flout))
+var numeroInteriro = -7
+var numeroFracionado = 5.5
+
+//boolean (true x false)
+var test = true
+
+//A inferencia do tipo da variavel é atribuido conforme o valor fornecido para a variavel.
+
+//alert(numeroFracionado) //open dialog
+//document.write(texto)
+console.log(numeroInteriro)
